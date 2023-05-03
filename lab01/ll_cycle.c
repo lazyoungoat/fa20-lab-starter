2aa34e912e1d853fb9bdd523969f95f948da9418
Nikita <l.r.shiun@gmail.com>
2023-05-03 13:51:24 +0800

    my failed attempt

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -1,7 +1,21 @@
 #include <stddef.h>
 #include "ll_cycle.h"
+#include <stdio.h>
 
 int ll_has_cycle(node *head) {
-    /* your code here */
+	node * tortoise;
+	node * hare;
+	
+
+	while((hare->next)!=NULL){
+		(hare->next)+=2;
+		if((hare->next) == NULL){
+			printf("acyclic");
+		}
+		(tortoise->next)++;
+		if((hare->next) == (tortoise->next)){
+			printf("cyclic");
+		}
+	}
     return 0;
 }
